919e7ee25d850b86b9e4a3f7e13d26289c23234f
Yang Liu <foamliu@yeah.net>
2017-11-12 06:28:09 +0800

    reinforcement learning visualization

--- rl/frozenLakeQ.py
+++ rl/frozenLakeQ.py
@@ -5,6 +5,7 @@ import gym
 import numpy as np
 import matplotlib.pyplot as plt
 from gym import wrappers
+import json
 
 num_episodes = 1000
 
@@ -31,6 +32,8 @@ def run_episode(env, Q, learning_rate, discount, episode, render=False):
 
         # Q(state, action) = R(state, action) + Gamma * Max[Q(next state, all actions)]
         Q[curr_state, action] += learning_rate * (reward + discount * np.max(Q[observation, :]) - Q[curr_state, action])
+        q_list.append(Q.tolist())
+        observation_list.append(observation)
 
     return Q, t_reward
 
@@ -44,8 +47,8 @@ def train():
         max_episode_steps=100,
         reward_threshold=0.78,  # optimum = .8196
     )
-    env = gym.make('FrozenLake-v0')
-    #env = gym.make('FrozenLakeNotSlippery-v0')
+    #env = gym.make('FrozenLake-v0')
+    env = gym.make('FrozenLakeNotSlippery-v0')
     env = wrappers.Monitor(env, '/tmp/FrozenLake-experiment-6', force=True)
     learning_rate = 0.81
     discount = 0.96
@@ -53,7 +56,7 @@ def train():
     reward_per_ep = list()
     Q = np.zeros((env.observation_space.n, env.action_space.n))
     for i in range(num_episodes):
-        Q, reward = run_episode(env, Q, learning_rate, discount, i, True)
+        Q, reward = run_episode(env, Q, learning_rate, discount, i, False)
         reward_per_ep.append(reward)
     # print "----------Next Episode---------"
     # print i
@@ -61,6 +64,13 @@ def train():
 
     return Q
 
+if __name__ == '__main__':
+    q_list = []
+    observation_list = []
+    q = train()
+    print(q)
 
-q = train()
-print(q)
+    f = open('data.json', 'w')
+    f.write(json.dumps({'q_list': q_list, 'ob_list': observation_list}))
+    f.close()
+    #print(json.dumps(observation_list))
