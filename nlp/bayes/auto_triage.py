c2b2791d869cc29b75c68e0764ea90e0a232b07b
Yang Liu <foamliu@microsoft.com>
2017-11-16 16:54:18 +0800

    update NB slide

--- nlp/bayes/auto_triage.py
+++ nlp/bayes/auto_triage.py
@@ -1,5 +1,9 @@
 import pandas as pd
 from textblob.classifiers import NaiveBayesClassifier
+from textblob.classifiers import MaxEntClassifier
+from textblob.classifiers import NLTKClassifier
+from textblob.classifiers import DecisionTreeClassifier
+import nltk
 
 def remove_prefix(theList, prefix):
     return [text[len(prefix):] for text in theList]
@@ -20,8 +24,35 @@ if __name__ == '__main__':
     print(cl.classify("[Support] [UCMA] For the removable tiles the colors of 'change - compared to' doesn't match positivity/negativity of change | UCM000000968815"))
 
     # Compute accuracy
-    print("Accuracy: {0}".format(cl.accuracy(test)))
+    print("NaiveBayes Accuracy: {0}".format(cl.accuracy(test)))
 
     # Show 10 most informative features
     cl.show_informative_features(10)
     print(cl.informative_features(10))
+
+    cl = DecisionTreeClassifier(train)
+    print("DecisionTree Accuracy: {0}".format(cl.accuracy(test)))
+    print(cl.pseudocode())
+
+    #cl = MaxEntClassifier(train, algorithm='megam', max_iter=10, trace=0)
+    #print("MaxEnt(megam) Accuracy: {0}".format(cl.accuracy(test)))
+
+    #cl = MaxEntClassifier(train, algorithm='gis', max_iter=10, trace=0, min_lldelta=0.5)
+    #print("MaxEnt(gis) Accuracy: {0}".format(cl.accuracy(test)))
+
+    from nltk.classify.scikitlearn import SklearnClassifier
+    from sklearn.naive_bayes import MultinomialNB,BernoulliNB
+
+    MNB_classifier = SklearnClassifier(MultinomialNB())
+    MNB_classifier.train(train)
+    print("MultinomialNB accuracy percent:", nltk.classify.accuracy(MNB_classifier, test))
+
+    BNB_classifier = SklearnClassifier(BernoulliNB())
+    BNB_classifier.train(train)
+    print("BernoulliNB accuracy percent:", nltk.classify.accuracy(BNB_classifier, test))
+
+
+
+
+
+
