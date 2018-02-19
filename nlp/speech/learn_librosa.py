d75e15f2eb379028795d98cde1056c33ac0bf6cd
Yang Liu <foamliu@yeah.net>
2018-02-19 22:43:00 +0800

    learn librosa

---
+++ nlp/speech/learn_librosa.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+import numpy as np
+
+y, sr = librosa.load(librosa.util.example_audio_file())
+librosa.output.write_wav('file_trim_5s.wav', y, sr)
+print(sr)
+print(len(y))
+
+plt.figure()
+plt.subplot(3, 1, 1)
+librosa.display.waveplot(y, sr=sr)
+plt.title('Monophonic')
+
+
+plt.figure(figsize=(12, 8))
+D = librosa.amplitude_to_db(librosa.stft(y), ref=np.max)
+plt.subplot(4, 2, 1)
+librosa.display.specshow(D, y_axis='linear')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Linear-frequency power spectrogram')
+
+plt.subplot(4, 2, 2)
+librosa.display.specshow(D, y_axis='log')
+plt.colorbar(format='%+2.0f dB')
+plt.title('Log-frequency power spectrogram')
+
+plt.show()
