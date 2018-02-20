045c64d93ef6c71df7dcca41ed90651c016f6a23
Yang Liu <foamliu@yeah.net>
2018-02-20 22:53:09 +0800

    it works now

--- tools/tSNE.py
+++ tools/tSNE.py
@@ -1,19 +1,10 @@
 import pandas as pd
-import numpy as np
-import tensorflow as tf
-import os
-
-print(__doc__)
 from time import time
-
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import offsetbox
 from sklearn import (manifold, datasets, decomposition, ensemble,
                      discriminant_analysis, random_projection)
-
-import matplotlib.pyplot as plt
-
 
 
 # Define a function to show image through 48*48 pixels
@@ -22,6 +13,7 @@ def show(img):
 
     # plt.imshow(show_image, cmap=cm.binary)
     plt.imshow(show_image, cmap='gray')
+    plt.show()
 
 
 def read_data(file):
@@ -59,30 +51,30 @@ def read_data(file):
 
 #----------------------------------------------------------------------
 # Scale and visualize the embedding vectors
-def plot_embedding(X, title=None):
-    x_min, x_max = np.min(X, 0), np.max(X, 0)
-    X = (X - x_min) / (x_max - x_min)
+def plot_embedding(X_tsne, title=None):
+    x_min, x_max = np.min(X_tsne, 0), np.max(X_tsne, 0)
+    X_tsne = (X_tsne - x_min) / (x_max - x_min)
 
     plt.figure()
     ax = plt.subplot(111)
-    for i in range(X.shape[0]):
-        plt.text(X[i, 0], X[i, 1], str(labels[i]),
+    for i in range(X_tsne.shape[0]):
+        plt.text(X_tsne[i, 0], X_tsne[i, 1], str(labels[i]),
                  color=plt.cm.Set1(labels[i] / 7.),
                  fontdict={'weight': 'bold', 'size': 9})
 
-    if hasattr(offsetbox, 'AnnotationBbox'):
-        # only print thumbnails with matplotlib > 1.0
-        shown_images = np.array([[1., 1.]])  # just something big
-        for i in range(labels.shape[0]):
-            dist = np.sum((X[i] - shown_images) ** 2, 1)
-            if np.min(dist) < 4e-3:
-                # don't show points that are too close
-                continue
-            shown_images = np.r_[shown_images, [X[i]]]
-            imagebox = offsetbox.AnnotationBbox(
-                offsetbox.OffsetImage(X[i].reshape((48,48)), cmap=plt.cm.gray_r),
-                X[i])
-            ax.add_artist(imagebox)
+    # if hasattr(offsetbox, 'AnnotationBbox'):
+    #     # only print thumbnails with matplotlib > 1.0
+    #     shown_images = np.array([[1., 1.]])  # just something big
+    #     for i in range(X_tsne.shape[0]):
+    #         dist = np.sum((X_tsne[i] - shown_images) ** 2, 1)
+    #         if np.min(dist) < 4e-3:
+    #             # don't show points that are too close
+    #             continue
+    #         shown_images = np.r_[shown_images, [X_tsne[i]]]
+    #         imagebox = offsetbox.AnnotationBbox(
+    #             offsetbox.OffsetImage(images[i].reshape(image_width,image_height), cmap=plt.cm.gray),
+    #             X_tsne[i])
+    #         ax.add_artist(imagebox)
     plt.xticks([]), plt.yticks([])
     if title is not None:
         plt.title(title)
@@ -99,12 +91,11 @@ if __name__ == '__main__':
     import pickle
     import os.path
 
-    fname = 'data.pkl'
-
     images, labels = read_data(file)
 
+    fname = '/Users/yangliu/code/deep_learning/tools/data.pkl'
     if os.path.isfile(fname):
-        with open(fname, 'r') as input:
+        with open(fname, 'rb') as input:
             X_tsne = pickle.load(input)
 
     else:
@@ -120,7 +111,7 @@ if __name__ == '__main__':
             pickle.dump(X_tsne, output)
 
     t0 = time()
-    plot_embedding(X_tsne,
+    plot_embedding(X_tsne[0:300],
                    "t-SNE embedding of the digits (time %.2fs)" %
                    (time() - t0))
 
