5ff85d734b2fbe07cdcfda511f9347e973ab0a67
Yang Liu <foamliu@microsoft.com>
2018-02-23 15:41:45 +0800

    init invoke

---
+++ cv/invokeCognitive.py
@@ -0,0 +1,15 @@
+
+import cognitive_face as CF
+
+KEY = 'cc0014b6b1934e2a92cd32c21de43fdb'  # Replace with a valid subscription key (keeping the quotes in place).
+CF.Key.set(KEY)
+# If you need to, you can change your base API url with:
+CF.BaseUrl.set('https://westcentralus.api.cognitive.microsoft.com/face/v1.0/')
+
+#BASE_URL = 'https://westus.api.cognitive.microsoft.com/face/v1.0/'  # Replace with your regional Base URL
+#CF.BaseUrl.set(BASE_URL)
+
+# You can use this example JPG or replace the URL below with your own URL to a JPEG image.
+img_url = 'http://13.65.250.1/image/facesign_0.png'
+faces = CF.face.detect(img_url)
+print(faces)
